6eec6ec14a8b021799ed2fd538aef95e92a75812
Chris <chewning.cl@gmail.com>
2018-07-24 21:16:41 -0500

    npmPackages.js file

---
+++ nodeJS.js/npmPackages.js
@@ -0,0 +1,35 @@
+
+
+NODE PACKAGE MANAGER (npm)
+the 'largest ecosystem of open source libraries in the world.'
+
+ APP.USE()
+ type app.use() to specify middleware as the callback function.
+
+
+npm start vs nodemon (or node server.js)
+
+
+NPM INIT
+Creates package.json. ... The default name for this file is index.js unless you created server.js or something else.js in not in a folder.
+
+PACKAGE.JSON
+All npm packages contain a file, usually in the project root, called package.json - this file holds various metadata relevant to the project. This file is used to give information to npm that allows it to identify the project as well as handle the project's dependencies
+
+
+
+"dependencies": {
+  "bcrypt": "^3.0.0",
+  "body-parser": "^1.18.3",
+  "chalk": "^2.4.1",
+  "ejs": "^2.6.1",
+  "express": "^4.16.3",
+  "express-session": "^1.15.6",
+  "foreman": "^3.0.1",
+  "helmet": "^3.13.0",
+  "heroku": "^7.7.4",
+  "method-override": "^3.0.0",
+  "mongodb": "^3.1.1",
+  "mongoose": "^5.2.5",
+  "nodemon": "^1.18.3"
+}
