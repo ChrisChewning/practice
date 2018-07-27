d9fde97fb2d98a32488193c61143188c6fb11c30
Chris <chewning.cl@gmail.com>
2018-07-27 13:41:18 -0500

    some css

---
+++ CSS/footer.js
@@ -0,0 +1,13 @@
+
+
+POSITIONS
+
+1. position: absolute; will move the footer when you scroll.
+
+2. position: fixed; If you want the footer to be always show at the bottom of the screen, change it to:
+position: fixed;
+
+3. position: relaitve; If you want the footer to stay at the bottom of the DOM.
+
+
+*If the footer, or a diff. child element keeps getting into a div about it, check you closed the divs in the html.
