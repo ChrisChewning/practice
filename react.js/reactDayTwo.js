90db0785d3fac3aa6cc5350d6d993ae3e601c1fd
Chris <chewning.cl@gmail.com>
2018-07-31 23:32:21 -0500

    api work

--- react.js/reactDayTwo.js
+++ react.js/reactDayTwo.js
@@ -11,17 +11,35 @@ ex: when you login, you want to be taken to a new screen.
 
 
 - Only thing to define inside the constructor is the state.
-- Don't define function inside the render
+- Dont define function inside the render
 - render can be above things (order doesn't matter) but it's nice to have it as the last code block.
 - To write javascript inside a return statement you have to wrap it in brackets {}
 
--to access any value, use state. This gives you taht value at any given time.
+-to access any value, use state. This gives you that value at any given time.
 - onChange can update our state too.
 - this.setState always calls a re-render. this is good with login forms.
 -setState is the last thing you want to do in your render since
 
 
-3. SUPER & INHERITING CLASSES
+3. STATE v. STATELESS
+Stateful components are always class components. As previously mentioned, stateful components have a state that gets initialized in the constructor.
+
+// Here is an excerpt from the counter example
+constructor(props) {
+  super(props);
+  this.state = { count: 0 };
+}
+
+Stateless components
+
+
+
+4. API LifeCycle Methods
+you have 3:
+Constructor, componentDidMount and Render.
+
+
+5. SUPER & INHERITING CLASSES
 
 *NOTE: You have to use the super() method if you are using the constructor() method AND extending the class. This is b.c the code is like "what are you extending from?"
 It simply executes the parent constructor.
