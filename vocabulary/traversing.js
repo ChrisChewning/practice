cf9b72f37fa3ee027acaa4a1266cb5588099418e
Chris <chewning.cl@gmail.com>
2018-09-08 22:35:41 -0500

    update

---
+++ vocabulary/traversing.js
@@ -0,0 +1,3 @@
+Traversing = to travel through.
+
+jQuery traversing, which means "move through", are used to "find" (or select) HTML elements based on their relation to other elements. ... With jQuery traversing, you can easily move up (ancestors), down (descendants) and sideways (siblings) in the tree, starting from the selected (current) element.
