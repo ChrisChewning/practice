c0ab7b91cf86ad8e587c63bdbc0ec1b5c965fc7d
Chris <chewning.cl@gmail.com>
2018-09-20 23:29:31 -0500

    updated react

--- react.js/dailyNotes/reactday11.js
+++ react.js/dailyNotes/reactday11.js
@@ -1,1 +1,3 @@
-//NOTE: call with Jim.
+webpack bundles multiple JavaScript files into one file for the browser.
+
+Babel will convert ES6 (ES2015) code to ES5, since most browsers don't yet understand all of ES6. As this article ages, browsers will support ES6 and Babel may not be needed.
