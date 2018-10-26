c9710461795d75fc65ec6d8777749977251928c8
Chris <chewning.cl@gmail.com>
2018-10-25 22:44:38 -0500

    async review 2

--- asyncFunctions.js/asyncAwait.js
+++ asyncFunctions.js/asyncAwait.js
@@ -32,15 +32,25 @@ function createPost(post){
 });
 }
 
-const promise1 = Promise.resolve('Hello World');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) =>
-  setTimeout(resolve, 2000, 'Goodbye'));
 
-//with fetch you need to do 2 .thens.  first: json. next: data
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json() );
-//without then .then() you get the metadata?
-//with the .then() you actually get the user data.
+//ASYNC / AWAIT
+//function has to be labeled async to use await. Await awaits for an asynchronous function or process to get done.
+async function init() {
+  await createPost({title: 'Post three', body: 'This is post three'});
+
+  getPosts();
+}
+
+init()
 
 
-Promise.all([promise1, promise2, promise3, promise4]).then(( (values) => console.log(values))); //takes in an array of promises
+//ASYNC / AWAIT using FETCH
+
+async function fetchUsers() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+
+  const data = await res.json();
+
+  console.log(data);
+}
+fetchUsers();
